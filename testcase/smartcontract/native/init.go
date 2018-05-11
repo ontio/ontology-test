6fbe0f7b2485e7d7bb5ab99774934d9b7cc81d4f
Alver Lyu <AlverLyu@gmail.com>
2018-05-11 17:39:38 +0800

    add ontid test case

--- testcase/smartcontract/native/init.go
+++ testcase/smartcontract/native/init.go
@@ -1,9 +1,11 @@
 package native
 
 import (
+	"github.com/ontio/ontology-test/testcase/smartcontract/native/ontid"
 	"github.com/ontio/ontology-test/testframework"
 )
 
 func TestNative() {
 	testframework.TFramework.RegTestCase("TestOntTransfer", TestOntTransfer)
+	ontid.TestNativeOntID()
 }
