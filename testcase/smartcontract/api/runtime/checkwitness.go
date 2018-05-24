2fae53b65f277cf3421815df5da1d4779475b29d
Hu Guanjun <huguanjun@onchain.com>
2018-05-24 18:22:38 +0800

    refactor runtime checkwitness testcase

--- testcase/smartcontract/api/runtime/checkwitness.go
+++ testcase/smartcontract/api/runtime/checkwitness.go
@@ -3,10 +3,10 @@ package runtime
 import (
 	"time"
 
+	"github.com/ontio/ontology-crypto/keypair"
+	"github.com/ontio/ontology-crypto/signature"
 	"github.com/ontio/ontology-go-sdk/utils"
 	"github.com/ontio/ontology-test/testframework"
-	"github.com/ontio/ontology/account"
-	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/smartcontract/types"
 )
 
@@ -18,16 +18,24 @@ using System.Numerics;
 
 public class A : SmartContract
 {
-    public static bool Main(byte[] input)
+    public static void Main(byte[] input)
     {
-        return Runtime.CheckWitness(input);
+
+
+        bool b = Runtime.CheckWitness(input);
+        if (b) {
+            Storage.Put(Storage.CurrentContext, "result", "true");
+        }
+        Storage.Put(Storage.CurrentContext, "result", "true");
     }
 }
+
+code = 53c56b6c766b00527ac4616c766b00c36168184e656f2e52756e74696d652e436865636b5769746e6573736c766b51527ac46c766b51c36c766b52527ac46c766b52c3643f00616168164e656f2e53746f726167652e476574436f6e7465787406726573756c740474727565615272680f4e656f2e53746f726167652e50757461616168164e656f2e53746f726167652e476574436f6e7465787406726573756c740474727565615272680f4e656f2e53746f726167652e50757461616c7566
 */
 
 func TestCheckWitness(ctx *testframework.TestFrameworkContext) bool {
-	code := "52c56b6c766b00527ac4616c766b00c36168184e656f2e52756e74696d652e436865636b5769746e6573736c766b51527ac46203006c766b51c3616c7566"
-	codeAddr := utils.GetNeoVMContractAddress(code)
+	code := "53c56b6c766b00527ac4616c766b00c36168184e656f2e52756e74696d652e436865636b5769746e6573736c766b51527ac46c766b51c36c766b52527ac46c766b52c3643f00616168164e656f2e53746f726167652e476574436f6e7465787406726573756c740474727565615272680f4e656f2e53746f726167652e50757461616168164e656f2e53746f726167652e476574436f6e7465787406726573756c740474727565615272680f4e656f2e53746f726167652e50757461616c7566"
+	codeAddress := utils.GetNeoVMContractAddress(code)
 	signer, err := ctx.GetDefaultAccount()
 
 	if err != nil {
@@ -40,7 +48,7 @@ func TestCheckWitness(ctx *testframework.TestFrameworkContext) bool {
 		types.NEOVM,
 		true,
 		code,
-		"TestCheckWitness",
+		"",
 		"",
 		"",
 		"",
@@ -58,35 +66,33 @@ func TestCheckWitness(ctx *testframework.TestFrameworkContext) bool {
 		return false
 	}
 
-	//checker, err := ctx.Wallet.CreateAccount()
-	//
-	//if err != nil {
-	//	ctx.LogError("TestCheckWitness - CreateAccount error: %s", err)
-	//	return false
-	//}
+	checker, err := ctx.Wallet.NewAccount("", keypair.PK_ECDSA, keypair.P256, signature.SHA256withECDSA, []byte("test"))
 
-	if !checkWitness(ctx, codeAddr, signer, signer, true) {
-		ctx.LogError("TestCheckWitness - checkwitness should return true.")
+	if err != nil {
+		ctx.LogError("TestCheckWitness generate account error:%s", err)
 		return false
 	}
 
-	return true
-}
-
-func checkWitness(ctx *testframework.TestFrameworkContext, codeAddress common.Address, caller, checker *account.Account, expect bool) bool {
-	res, err := ctx.Ont.Rpc.InvokeNeoVMSmartContract(ctx.GetGasPrice(), ctx.GetGasLimit(),
-		caller,
+	_, err = ctx.Ont.Rpc.InvokeNeoVMSmartContract(ctx.GetGasPrice(), ctx.GetGasLimit(),
+		signer,
 		0,
 		codeAddress,
-		[]interface{}{checker.Address})
-
+		[]interface{}{checker.Address[:]})
 	if err != nil {
-		ctx.LogError("TestCheckWitness error: %s", err)
+		ctx.LogError("TestDomainSmartContract InvokeNeoVMSmartContract error: %s", err)
+	}
+
+	ctx.Ont.Rpc.WaitForGenerateBlock(30*time.Second, 1)
+
+	res, err := ctx.Ont.Rpc.GetStorage(codeAddress, []byte("result"))
+	if err != nil {
+		ctx.LogError("TestDomainSmartContract GetStorageItem key:hello error: %s", err)
 		return false
 	}
-	err = ctx.AssertToBoolean(res, expect)
+
+	err = ctx.AssertToString(string(res), "true")
 	if err != nil {
-		ctx.LogError("TestCheckWitness AssertToBoolean error:%s", err)
+		ctx.LogError("TestDomainSmartContract AssertToString error: %s", err)
 		return false
 	}
 	return true
